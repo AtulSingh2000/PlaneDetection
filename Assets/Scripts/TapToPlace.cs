80dcaae08503ecebd7738dbc707b4234261becc7
AtulSingh2000 <atulkumar102s@gmail.com>
2020-06-24 15:28:28 +0530

    tap to place object script added

---
+++ Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class TapToPlace : MonoBehaviour
+{
+    public GameObject ObjectToInstantiate;
+
+    private GameObject SpawnedObject;
+
+    private ARRaycastManager aRRaycastManager;
+
+    private Vector2 TouchPosition;
+
+    static List<ARRaycastHit> Hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GetTouchPos(out Vector2 TouchPosition))
+            return;
+        if(aRRaycastManager.Raycast(TouchPosition, Hits, TrackableType.PlaneWithinPolygon))
+        {
+            var HitPos = Hits[0].pose;
+
+            if(SpawnedObject == null)
+            {
+                SpawnedObject = Instantiate(ObjectToInstantiate, HitPos.position, HitPos.rotation);
+            }
+            else
+            {
+                SpawnedObject.transform.position = HitPos.position;
+            }
+        }
+    }
+
+
+    bool GetTouchPos(out Vector2 TouchPosition)
+    {
+        if(Input.touchCount>0)
+        {
+            TouchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        TouchPosition = default;
+        return false;
+    }
+}
